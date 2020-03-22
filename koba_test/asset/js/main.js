fb785c3759a9e9e7213b1a09ff1a794cb1e32187
bpi_kobayakawa <c.kobayakawa@bpi.yokohama>
2020-03-22 17:23:54 +0900

    あああ

---
+++ koba_test/asset/js/main.js
@@ -0,0 +1,15 @@
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    name: 'うおおおおおおおお'
+  }
+})
+
+new Vue({
+    el: '#app',
+    vuetify: new Vuetify(),
+    data: () => ({
+      drawer: null
+    })
+  })
